00e5cfa3e1f79a2a321881565e23ea84480d5ace
Jiahao Yu <ilywanzi@gmail.com>
2017-09-09 23:55:08 -0700

    second

--- ninja_gold.py
+++ ninja_gold.py
@@ -15,24 +15,34 @@ def landing():
 @app.route("/process_money", methods = ['POST'])
 def find_gold():
 	if request.form['building'] == 'farm':
-		session['result'] += random.randint(10,20)
-		# session['message'].append('Earned ' + str(session['result']) + ' golds from the farm!(' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ')')
-		return render_template("ninja_gold.html")
+		session['newearn'] = random.randint(10,20)
+		session['result'] += session['newearn']
+		session['message'].append('Earned ' + str(session['newearn']) + ' golds from the farm!(' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ')')
+		return redirect("/")
 	
-	# if request.form['building'] == 'cave':
-	# 	session['result'] += random.randint(10,20)
-	# 	# session['message'].append('Earned ' + str(session['result']) + ' golds from the farm!(' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ')')
-	# 	return render_template("ninja_gold.html")
+	if request.form['building'] == 'cave':
+		session['newearn'] = random.randint(5,10)
+		session['result'] += session['newearn']
+		session['message'].append('Earned ' + str(session['newearn']) + ' golds from the cave!(' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ')')
+		return redirect("/")
 
-	# if request.form['building'] == 'house':
-	# 	session['result'] += random.randint(10,20)
-	# 	# session['message'].append('Earned ' + str(session['result']) + ' golds from the farm!(' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ')')
-	# 	return render_template("ninja_gold.html")
+	if request.form['building'] == 'house':
+		session['newearn'] = random.randint(2,5)
+		session['result'] += session['newearn']
+		session['message'].append('Earned ' + str(session['newearn']) + ' golds from the house!(' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ')')
+		return redirect("/")
 
-	# if request.form['building'] == 'casino':
-	# 	session['result'] += random.randint(10,20)
-	# 	# session['message'].append('Earned ' + str(session['result']) + ' golds from the farm!(' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ')')
-		# return render_template("ninja_gold.html")
+	if request.form['building'] == 'casino':
+		session['newearn'] = random.randint(-50,50)
+		session['result'] += session['newearn']
+		if session['newearn'] >= 0:
+			session['message'].append('Entered a casino and earned ' + str(session['newearn']) + ' golds from the casino!(' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ')')
+		elif session['newearn'] < 0:
+			if session['result'] >= 0:
+				session['message'].append('Entered a casino and lost ' + str(abs(session['newearn'])) + ' golds from the casino!(' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ')')
+			elif session['result'] < 0:
+				session['message'].append('Now you owe us ' + str(session['result']) + ' gold!')
+		return redirect("/")
 
 @app.route('/reset', methods = ['POST'])
 def reset():
